876ba41cf3020a7fab2cd31645d1b9df0bc71b70
“lizefeng” <280796942@qq.com>
2024-06-21 10:51:54 +0800

    通用布局

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import ArcoVue from "@arco-design/web-vue";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(ArcoVue).use(router).mount("#app");
